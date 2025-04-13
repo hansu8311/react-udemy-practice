905faa2213a1626123564890463f6001594a9a6f
DESKTOP-120P58C\user <cjsdyd453@naver.com>
2025-04-13 16:23:19 +0900

    useEffect의 Cleanup 함수 소개

--- side-effect-useeffect/src/components/DeleteConfirmation.jsx
+++ side-effect-useeffect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
