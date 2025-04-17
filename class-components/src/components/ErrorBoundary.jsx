da5c0e59b271ea46d675e36dedec2cbe030634b1
kimhansu <cjsdyd453@naver.com>
2025-04-17 13:11:21 +0900

    class-components : ErrorBoundary

---
+++ class-components/src/components/ErrorBoundary.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasError: false,
+    };
+  }
+  componentDidCatch(error) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Soomthing went worng</p>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
