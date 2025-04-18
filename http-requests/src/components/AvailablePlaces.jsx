cfb27136685f824ca4b6b0f3ac4aaac51fb06aba
kimhansu <cjsdyd453@naver.com>
2025-04-18 11:15:33 +0900

    HTTP Requests

--- http-requests/src/components/AvailablePlaces.jsx
+++ http-requests/src/components/AvailablePlaces.jsx
@@ -3,11 +3,14 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const res = await fetch("http://localhost:3000/places");
       const { places } = await res.json();
       setAvailablePlaces(places);
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -15,6 +18,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place date..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
