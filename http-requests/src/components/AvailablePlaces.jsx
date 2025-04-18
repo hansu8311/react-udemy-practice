cac6f90c5574332da90d7372bc500a3d5c60b856
kimhansu <cjsdyd453@naver.com>
2025-04-18 10:36:07 +0900

    HTTP Requests

--- http-requests/src/components/AvailablePlaces.jsx
+++ http-requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,20 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    async function getPlaces() {
+      const res = await fetch("http://localhost:3000/places");
+      const { places } = await res.json();
+      setAvailablePlaces(places);
+    }
+    getPlaces();
+  }, []);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
