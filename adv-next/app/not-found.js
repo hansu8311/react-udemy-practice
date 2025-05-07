a4c10942c95851bf613372caa32cc9082a9629c1
kimhansu <cjsdyd453@naver.com>
2025-05-07 10:39:37 +0900

    adv-next-app - not-found.js 파일

---
+++ adv-next/app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound(props) {
+  return (
+    <main>
+      <h1>Not found</h1>
+      <p>Unfortunatel, we clould not find the page</p>
+    </main>
+  );
+}
+
+export default NotFound;
