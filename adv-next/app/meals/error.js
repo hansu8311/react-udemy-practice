58bf4863b821a726f54eaa71745ac8c2890b1168
kimhansu <cjsdyd453@naver.com>
2025-05-07 10:38:16 +0900

    adv-next-app - error.js 파일

---
+++ adv-next/app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+function ErrorPage() {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Fail to fetch data. Please try again.</p>
+    </main>
+  );
+}
+
+export default ErrorPage;
