06779d0f27a9d857a067a930ba2cd7f73de5f4e2
kimhansu <cjsdyd453@naver.com>
2025-05-07 11:00:27 +0900

    adv-next-app - nextjs notFound();

---
+++ adv-next/app/meals/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound(props) {
+  return (
+    <main>
+      <h1>Meal Not found</h1>
+      <p>Unfortunatel, we clould not find meal data</p>
+    </main>
+  );
+}
+
+export default NotFound;
