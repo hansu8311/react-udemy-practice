3e94142bd2c00a26709d4ac0d56dfc862fc9d22b
kimhansu <cjsdyd453@naver.com>
2025-05-12 13:53:31 +0900

    eact-testing -jest, testing-library/react : jest 를 활용하여 mock 데이터 리턴하는 테스트 작성

--- react-testing/src/components/Async.test.js
+++ react-testing/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 import userEvent from "@testing-library/user-event";
 describe("Async components", () => {
   test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "p1" }],
+    });
     //Arrange
     render(<Async />);
     //Assert;
